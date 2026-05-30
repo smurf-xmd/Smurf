0ee498a5f0365824c1e72c68f1d299a3ee9d91de
SMURF-XMD <mythicfusion65@gmail.com>
2026-05-30 15:25:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
     PORT: process.env.PORT || 50900,
-    SESSION_PREFIX: process.env.SESSION_PREFIX || "GURU~",
-    GC_JID: process.env.GC_JID || "LZE4CoZNhLB28z5jtqwNLA",
+    SESSION_PREFIX: process.env.SESSION_PREFIX || "SMURF~",
+    GC_JID: process.env.GC_JID || "HRdlOPWj9lyBIEfLC3PPU6",
     DATABASE_URL: process.env.DATABASE_URL || "",
-    BOT_REPO: process.env.BOT_REPO || "https://github.com/GuruhTech/ULTRA-GURU",
-    WA_CHANNEL: process.env.WA_CHANNEL || "https://whatsapp.com/channel/0029VbCl2UX3rZZilMSvxN1e",
-    MSG_FOOTER: process.env.MSG_FOOTER || "> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ GuruTech ᴛᴇᴄʜ*",
+    BOT_REPO: process.env.BOT_REPO || "https://github.com/smurf-xmd/Smurf",
+    WA_CHANNEL: process.env.WA_CHANNEL || "https://chat.whatsapp.com/HRdlOPWj9lyBIEfLC3PPU6",
+    MSG_FOOTER: process.env.MSG_FOOTER || "> *Powered by cyberquest*",
 };
